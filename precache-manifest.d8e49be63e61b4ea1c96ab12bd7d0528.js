19225e34c5bc1952116554f1d950b1647eca04ef
Jovana Vujic <vujicjovana44@gmail.com>
2020-02-25 23:05:01 +0100

    Updates

---
+++ precache-manifest.d8e49be63e61b4ea1c96ab12bd7d0528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9bd196701c985b7213da40f4aebcfe0",
+    "url": "/eDiary-app/static/media/linen-background.c9bd1967.jpg"
+  },
+  {
+    "revision": "c2063ad167b18aa7bc07",
+    "url": "/eDiary-app/static/js/runtime~main.ffee2303.js"
+  },
+  {
+    "revision": "2a815752eebd3914d32f",
+    "url": "/eDiary-app/static/js/main.71e3ab50.chunk.js"
+  },
+  {
+    "revision": "81881230794af4b1ecb3",
+    "url": "/eDiary-app/static/js/2.b7b3238e.chunk.js"
+  },
+  {
+    "revision": "2a815752eebd3914d32f",
+    "url": "/eDiary-app/static/css/main.9d76e296.chunk.css"
+  },
+  {
+    "revision": "bb251b9b25b088289228650f9ea4d1f9",
+    "url": "/eDiary-app/index.html"
+  }
+];
